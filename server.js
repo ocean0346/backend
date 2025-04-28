a8e61b2d29f64cf0b652374eedf1d540d00ce9d7
Duong <pengu0163@gmail.com>
2025-04-28 22:11:09 +0700

    1

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000','https://frontend-vv96.onrender.com',], // Nguồn gốc của frontend
+  origin: '*', // Nguồn gốc của frontend
   credentials: true, // Cho phép credentials (cookies, sessions)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -33,7 +33,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 // Session middleware
 app.use(
   session({
