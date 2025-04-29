f553fae7d779ac5c6eec589c19e06100af703287
Duong <pengu0163@gmail.com>
2025-04-29 09:29:40 +0700

    1

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.options('*', cors());
 // Session middleware
 app.use(
   session({
